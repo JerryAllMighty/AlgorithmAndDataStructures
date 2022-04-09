b62f8037e751b4969018dca10c29730e036b021f
김윤민 <dbsalszz@naver.com>
2022-04-09 15:01:39 +0900

    [FEAT] BJ 11021번 문제풀이 추가

---
+++ BaekJun/Math/BJ11021.py
@@ -0,0 +1,5 @@
+import sys
+cnt = int(sys.stdin.readline().rstrip())
+for i in range(1, cnt+1):
+  a, b = map(int, sys.stdin.readline().rstrip().split())
+  print(str.format("Case #{}: {}", i, a+b))
