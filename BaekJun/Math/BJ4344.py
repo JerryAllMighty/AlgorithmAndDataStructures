f59bc4acd6d3135e8f6c6ab0bf00f7139c256c45
김윤민 <dbsalszz@naver.com>
2022-04-10 01:24:15 +0900

    [FEAT] BJ4344 문제풀이 추가

---
+++ BaekJun/Math/BJ4344.py
@@ -0,0 +1,15 @@
+import sys
+import statistics
+import math
+cnt = int(sys.stdin.readline().rstrip())
+
+for _ in range(cnt):
+  each = list(map(int, sys.stdin.readline().rstrip().split()))
+  avg = statistics.mean(each[1:])
+  over = 0
+  for i in range(1, len(each)):
+    if each[i] > avg:
+      over += 1
+  
+  print(f"{((over/(len(each)-1))*100):.3f}")
+
