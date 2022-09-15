be4c0b1f7115056cccf32da006b18309bafdc7de
AzureAD\yoonminkim <dbsalszz@exit365.onmicrosoft.com>
2022-09-15 10:25:14 +0900

    [FEAT] BJ4104 문제풀이 추가

---
+++ BaekJun/implementation/BJ4101.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        a,  b = list(map(int, input().split(' ')))
+        if a == 0 and b == 0:
+            break
+        
+        if a > b:
+            print('Yes')
+        else:
+            print('No')
+    except EOFError:
+        break
