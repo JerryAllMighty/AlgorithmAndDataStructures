dc7871eb9249ae41f18ad5c85cc233c9f9764253
JerryAllMighty <dbsalszz@naver.com>
2022-09-29 10:11:46 +0900

    [FEAT] BJ2745 문제풀이 추가

---
+++ BaekJun/implementation/BJ2745.py
@@ -0,0 +1,35 @@
+given = input()
+
+
+
+if given == 'A+':
+    print(4.3)
+elif given == 'A0':
+    print(4.0)
+elif given == 'A-':
+    print(3.7)
+
+
+elif given == 'B+':
+    print(3.3)
+elif given == 'B0':
+    print(3.0)
+elif given == 'B-':
+    print(2.7)
+
+elif given == 'C+':
+    print(2.3)
+elif given == 'C0':
+    print(2.0)
+elif given == 'C-':
+    print(1.7)
+
+elif given == 'D+':
+    print(1.3)
+elif given == 'D0':
+    print(1.0)
+elif given == 'D-':
+    print(0.7)
+
+elif given == 'F':
+    print(0.0)
