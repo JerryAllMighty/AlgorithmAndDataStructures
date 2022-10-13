0921f2f318bcb0ea4187c78ac0ff79800ef9d898
JerryAllMighty <dbsalszz@naver.com>
2022-10-13 17:41:27 +0900

    [FEAT] 프로그래머스 최소직사각형 문제풀이 추가

--- Programmers/MinimumSquare.py
+++ Programmers/MinimumSquare.py
@@ -1,21 +1,12 @@
 def solution(sizes):
-    x = max([i[0] for i in sizes])
-    y = max([i[1] for i in sizes])
+    row = 0
+    col = 0
+    for a, b in sizes:
+        if a < b:
+            a, b = b, a
+        row = max(row, a)
+        col = max(col, b)
+    return row * col
 
-    for j in range(len(sizes)):
-        garo = [i[0] for i in sizes]
-        sero = [i[1] for i in sizes]
-        garo[j], sero[j] = sero[j], garo[j]
-
-        tempX = max(garo)
-        tempY = max(sero)
-
-        x = min(x, tempX)
-        y = min(y, tempY)
-
-
-    return x * y
-
-
-print(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]))
+print(solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]))
 
