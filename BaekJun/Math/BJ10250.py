4dbb1f920306fce4e05c74d16bf22c7f8e02c529
김윤민 <dbsalszz@naver.com>
2023-01-01 23:41:12 +0900

    [FEAT] BJ10250 문제풀이 추가

--- BaekJun/Math/BJ10250.py
+++ BaekJun/Math/BJ10250.py
@@ -3,8 +3,9 @@ answers = []
 for _ in range(cnt):
   h, w, n = map(int, input().split())
   a = (n % h)
-  b = (n // h)+1
+  b = (n // h) + 1
   if (n % h == 0):
+    b -= 1
     a = h
     
   answers.append((a*100)+b)
