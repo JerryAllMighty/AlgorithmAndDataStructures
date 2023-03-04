c20f64dcb95f807be3e1771890bd04b734dc1878
김윤민 <dbsalszz@naver.com>
2023-03-05 00:36:19 +0900

    [DP] BJ19947 문제풀이 추가

---
+++ BaekJun/DynamicProgramming/BJ19947.py
@@ -0,0 +1,12 @@
+h,y=map(int,input().split())
+
+money=[0 for i in range(11)]
+money[0]=h
+money[1]=int(h*1.05)
+money[2]=int(money[1]*1.05)
+money[3]=int(h*1.20)
+money[4]=max(int(money[3]*1.05), int(money[1]*1.1))
+
+for i in range(5,y+1):
+    money[i]=max(int(money[i-5]*1.35),int(money[i-3]*1.20),int(money[i-1]*1.05))
+print(money[y])
