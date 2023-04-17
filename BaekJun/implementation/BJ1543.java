b98e0a4214dfaa96e0721f4d08943706b2df4bfc
김윤민 <dbsalszz@naver.com>
2023-04-17 22:08:43 +0900

    [문자열] BJ1543 문제풀이 추가

---
+++ BaekJun/implementation/BJ1543.java
@@ -0,0 +1,25 @@
+package BaekJun.implementation;
+
+import java.util.Scanner;
+
+public class BJ1543 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String doc = sc.nextLine();
+    String word = sc.nextLine(); 
+    sc.close();
+    int count = 0;
+    int startIndex = 0;
+
+    while (true){
+      int findIndex = doc.indexOf(word, startIndex);
+      if (findIndex < 0 ){
+        break;
+      }
+      count++;
+      startIndex = findIndex + word.length();
+
+    }
+    System.out.println(count);
+  }
+}
