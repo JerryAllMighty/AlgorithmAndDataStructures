697d68be923e17be205af68de5eb35fe85cc5e5e
김윤민 <dbsalszz@naver.com>
2023-04-27 08:42:56 +0900

    [배열] BJ3273 문제풀이 추가

---
+++ BaekJun/implementation/BJ3273.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+import javax.swing.text.html.CSS;
+
+public class Main{
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int[] a = new int[N];
+
+    for(int i = 0; i < N; i++)
+      a[i] = sc.nextInt();
+    int X = sc.nextInt();
+
+    int[] cnt = new int[1000001];
+    for(int i = 0; i< N; i++)
+      cnt[a[i]]++;
+
+    long ans = 0;
+    for(int i = 0; i <= (X - 1)/2; i++)
+      if(X - i <= 1000000)
+        ans += (long)cnt[i] * cnt[X - i];
+
+
+
+}
+}
