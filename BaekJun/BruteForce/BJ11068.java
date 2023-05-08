289a907f9369227fe95b1cdb0d0b837d35d6dd5d
김윤민 <dbsalszz@naver.com>
2023-05-09 08:37:08 +0900

    [BruteForce] BJ11068 문제풀이

--- BaekJun/BruteForce/BJ11068.java
+++ BaekJun/BruteForce/BJ11068.java
@@ -6,6 +6,25 @@ public class BJ11068{
     Scanner sc = new Scanner(System.in);
     int cnt = sc.nextInt();
     int[] answers = new int[cnt];
+
+    private Boolean isPalindrome(int num, int jb){ 
+        String jbNum = "";
+        while(num >= jb){
+          int nmg = num %jb;
+          jbNum = String.valueOf(nmg) + jbNum;
+          num = num/jb;
+
+        }
+        String cmp = "";
+        for(int i =jbNum.length()-1; i >= 0; i--){
+          cmp += jbNum[i];
+
+        }
+
+        return jbNum.equals(cmp) ? true:false;
+    }
+
+
     for(int i = 0; i< cnt; i++){
       int num = sc.nextInt();
       int jb =1;
