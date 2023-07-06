f1995abb75371d28d68c261a60560fb7bc9537ea
yoonmin <dbsalszz@ex-it.co.kr>
2023-07-06 10:29:24 +0900

    [구현] BJ3085

--- BaekJun/implementation/BJ3085.py
+++ BaekJun/implementation/BJ3085.py
@@ -8,15 +8,42 @@ for _ in range(cnt):
 for row in range(cnt):
     for col in range(cnt):
         # we choose 2 with different colors
-        # row and column
-        if col != cnt - 1 and targets[row][col] != targets[row][col+1]:
-
 
         # row and row
+        if col != cnt - 1 and targets[row][col] != targets[row][col+1]:
+            targets[row][col], targets[row][col+1] = targets[row][col+1], targets[row][col]
+        # row and column
         if row != cnt-1 and targets[row][col] != targets[row+1][col]:
-            targets[row+1], targets[row] = targets[row], targets[row+1]
+            targets[row][col], targets[row + 1][col] = targets[row+1][col], targets[row][col]
+
+# find the longest consecutive
+answer = 0
+
+longestRowList = []
+longestColList = []
+
+for row in range(cnt):
+    longestRow = 0
+    for col in range(cnt):
+        longestCol = 0
+
+        # find the longest row
+        if col != cnt-1 and targets[row][col] == targets[row][col+1]:
+            longestRow += 1
+        else:
+            longestRowList.append(longestRow)
+            longestRow = 0
+
+        # find the longest col
+        if row != cnt - 1 and targets[row][col] == targets[row+1][col]:
+            longestCol += 1
+        else:
+            longestColList.append(longestCol)
+            longestCol = 0
 
 
-    # we change them.
+
+print(max(longestRowList), max(longestColList))
+
 
 
