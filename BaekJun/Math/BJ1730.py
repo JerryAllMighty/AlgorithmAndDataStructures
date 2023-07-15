66ec2c4294275f55413d56d838688666aa09f3c6
김윤민 <dbsalszz@naver.com>
2023-07-15 17:16:41 +0900

    [Math] BJ1730 final

--- BaekJun/Math/BJ1730.py
+++ BaekJun/Math/BJ1730.py
@@ -7,30 +7,36 @@ row = 0
 col = 0
 
 def move(row, col, dir):
-    if lst[row][col] == '.' and (dir == 'U' or dir == 'D'):
-        lst[row][col] = '|'
-    elif lst[row][col] == '.' and (dir == 'L' or dir == 'R'):
-        lst[row][col] = '-'
-    elif (lst[row][col] == '|' and dir == 'L' or dir == 'R') or (lst[row][col] == '-' and dir == 'U' or dir == 'D'):
-        lst[row][col] = '+'
-
+    cur = lst[row][col]
+    if cur == '.':
+        if dir == 'U' or dir == 'D':
+            lst[row][col] = '|'
+        elif dir == 'L' or dir == 'R':
+            lst[row][col] = '-'
+    elif cur == '|':
+        if dir == 'L' or dir == 'R':
+            lst[row][col] = '+'
+    elif cur == '-':
+        if dir == 'U' or dir == 'D':
+            lst[row][col] = '+'
 for i in cmd:
-    if i == 'U':
-        if row > 0:
-            move(row, col, i)
-            row -= 1
-    elif i == 'D':
-        if row < cnt-1:
-            move(row, col, i)
-            row += 1
-    elif i == 'L':
-        if col > 0:
-            move(row, col, i)
-            col -= 1
-    elif i == 'R':
-        if col < cnt-1:
-            move(row, col, i)
-            col += 1
+    if i == 'U' and row > 0:
+        move(row, col, i)
+        move(row-1, col, i)
+        row -= 1
+    elif i == 'D' and row < cnt-1:
+        move(row, col, i)
+        move(row + 1, col, i)
+        row += 1
+    elif i == 'L' and col > 0:
+        move(row, col, i)
+        move(row, col-1, i)
+        col -= 1
+    elif i == 'R' and col < cnt - 1:
+        move(row, col, i)
+        move(row, col+1, i)
+        col += 1
 
 
-print(lst)
+for i in lst:
+    print(''.join(i))
