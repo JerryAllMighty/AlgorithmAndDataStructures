5dfaaee5eb88886d3840b7e7c876f1be70c4d97c
김윤민 <dbsalszz@naver.com>
2023-07-18 21:05:38 +0900

    [Implementation] BJ2840 second time

--- BaekJun/implementation/BJ2840.py
+++ BaekJun/implementation/BJ2840.py
@@ -1,6 +1,20 @@
-kanCnt, dolCnt = list(map(int, input()))
-lst = []
-
+kanCnt, dolCnt = list(map(int, input().split(' ')))
+lst = ['*' for _ in range(kanCnt)]
+cur = 0
 for _ in range(dolCnt):
     changedCnt, letter = input().split(' ')
+    # calculate where the number stands the next time
+    idx = kanCnt - abs(cur - int(changedCnt))
+    cur = idx
+    if lst[idx] == '*':
+        lst[idx] = letter
+    else:
+        lst = '!'
+
+
+for i in lst:
+    if i == '*':
+        print('?')
+    else:
+        print(i)
 
