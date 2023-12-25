228a66a5e5a7f23cd12621c4589ea9ecdbcef34a
dbsalszz-admin <dbsalszz@naver.com>
2023-12-25 12:52:24 +0900

    [구현] BJ10431 다른 풀이 추가

--- BaekJun/implementation/BJ10431.java
+++ BaekJun/implementation/BJ10431.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Main
@@ -30,3 +31,51 @@ class Main
         }
     }
 }
+
+//2023.12.25 다른 풀이도 추가
+//
+//import java.lang.reflect.Array;
+//import java.util.ArrayList;
+//import java.util.Scanner;
+//
+//import static java.lang.Math.max;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int cnt = sc.nextInt();
+//        sc.nextLine();
+//        ArrayList<String> answerList = new ArrayList<String>();
+//
+//        for (int i = 1; i < cnt + 1; i++) {
+//            int answer = 0;
+//            String[] each = sc.nextLine().split(" ");
+//            ArrayList<Integer> tempList = new ArrayList<Integer>();
+//
+//            for (int j = 1; j < each.length; j++) {
+//                //비교 숫자
+//                int cmp = Integer.valueOf(each[j]);
+//                int idx = 0;
+//                for (int k = 0; k < tempList.size(); k++) {
+//                    int tempNum = tempList.get(k);
+//                    if (cmp < tempNum) {
+//                        answer += tempList.size() - k;
+//                        idx = k;
+//                        break;
+//                    } else {
+//                        idx = k + 1;
+//                    }
+//                }
+//                tempList.add(idx, cmp);
+//
+//            }
+//            answerList.add(Integer.toString(i) + " " + answer);
+//        }
+//
+//        for (int i = 0; i < answerList.size(); i++) {
+//            System.out.println(answerList.get(i));
+//        }
+//
+//        sc.close();
+//    }
+//}
