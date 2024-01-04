8b3df3850e359647e9a4c853990efc432652ebaf
dbsalszz-admin <dbsalszz@naver.com>
2024-01-04 23:28:56 +0900

    [Math] BJ 10158 문제풀이 추가

---
+++ BaekJun/Math/BJ10158.java
@@ -0,0 +1,22 @@
+package BaekJun.Math;
+
+import java.util.Scanner;
+
+public class BJ10158 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int W = sc.nextInt();
+        int H = sc.nextInt();
+        int P = sc.nextInt();
+        int Q = sc.nextInt();
+        int T = sc.nextInt();
+
+        int currentX = (T + P) % (2 * W);
+        int currentY = (T + Q) % (2 * H);
+        if(currentX > W) currentX = 2 * W - currentX;
+        if(currentY > H) currentY = 2 * H - currentY;
+
+        System.out.println(currentX + " " + currentY);
+        sc.close();
+    }
+}
