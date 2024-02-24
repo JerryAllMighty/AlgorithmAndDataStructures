e8540ec5633e8f1fe29053cadcf59d4c8834f63f
JerryAllMighty <dbsalszz@naver.com>
2024-02-24 17:26:14 +0900

    [Brute Force] BJ 11005 다른 문제풀이 시도

--- BaekJun/BruteForce/BJ11005.java
+++ BaekJun/BruteForce/BJ11005.java
@@ -39,4 +39,42 @@ public class BJ11005{
 
     
   }
+
+  /*2024.02.24
+  다른 풀이 시도, 시존보다 성능 안 좋음
+   */
+//  import java.util.Scanner;
+
+//  public class Main {
+//    public static void main(String[] args) {
+//      Scanner sc = new Scanner(System.in);
+//      String[] given = sc.nextLine().split(" ");
+//      int toChange = Integer.parseInt(given[0]);
+//      int jb = Integer.parseInt(given[1]);
+//
+//      char[] overTens = new char[36];
+//      for(int i = 0; i < 36;i++){
+//        if(i >= 10){
+//          overTens[i] = (char)(i+55);
+//        }else{
+//          overTens[i] = Character.forDigit(i, 10);
+//        }
+//
+//      }
+//
+//      String answer = "";
+//      while(true){
+//        if(toChange >= jb){
+//          answer = overTens[toChange%jb] + answer;
+//          toChange = toChange/jb;
+//        }else{
+//          answer = overTens[toChange] + answer;
+//          break;
+//        }
+//      }
+//
+//      System.out.println(answer);
+//    }
+//  }
+
 }
