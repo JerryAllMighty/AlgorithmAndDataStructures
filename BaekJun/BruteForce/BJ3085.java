b8278d3141b954fa82286bff44150f3ec4f45802
dbsalszz-admin <dbsalszz@naver.com>
2024-03-02 22:55:16 +0900

    [BruteForce] BJ3085 문제풀이 추가

---
+++ BaekJun/BruteForce/BJ3085.java
@@ -0,0 +1,73 @@
+package BaekJun.BruteForce;
+
+import java.util.Scanner;
+
+public class BJ3085 {
+    public static int findMaxRow(char[][] map) {
+        int N = map.length;
+        int maxRow = 0;
+        for (int r = 0; r < N; r++) {
+            int len = 1;
+            for (int c = 0; c < N; c++) {
+                if (map[r][c] == map[r][c - 1]) len++;
+                else {
+                    maxRow = Math.max(maxRow, len);
+                    len = 1;
+                }
+            }
+            maxRow = Math.max(maxRow, len);
+        }
+        return maxRow;
+    }
+
+    public static int findMaxColumn(char[][] map) {
+        int N = map.length;
+        int maxCol = 0;
+        for (int c = 0; c < N; c++) {
+            int len = 1;
+            for (int r = 1; r < N; r++) {
+                if (map[r][c] == map[r-1][c]) len++;
+                else {
+                    maxCol = Math.max(maxCol, len);
+                    len = 1;
+                }
+            }
+            maxCol = Math.max(maxCol, len);
+        }
+        return maxCol;
+    }
+
+    public static void swapCandy(char[][] map, int r1, int c1, int r2, int c2) {
+        char temp = map[r1][c1];
+        map[r1][c1] = map[r2][c2];
+        map[r2][c2] = temp;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        char[][] map = new char[N][N];
+        for (int i = 0; i < N; i++)
+            map[i] = sc.next().toCharArray();
+
+        int ans = 0;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) {
+                if (j + 1 < N && map[i][j] != map[i][j + 1]) {
+                    swapCandy(map, i, j, i, j + 1);
+                    ans = Math.max(ans, Math.max(findMaxColumn(map), findMaxRow(map)));
+                    swapCandy(map, i, j, i, j + 1);
+                }
+
+                if (i + 1 < N && map[i][j] != map[i + 1][j]) {
+                    swapCandy(map, i, j, i, j + 1);
+                    ans = Math.max(ans, Math.max(findMaxColumn(map), findMaxRow(map)));
+                    swapCandy(map, i, j, i, j + 1);
+
+                }
+            }
+
+        System.out.println(ans);
+    }
+}
