5e3b00606a0394a434b9672fae91c77c9607d123
dbsalszz-admin <dbsalszz@naver.com>
2024-07-03 22:37:45 +0900

    [Implementation] BJ15596 문제풀이 추가

--- BaekJun/Math/BJ15596.py
+++ BaekJun/Math/BJ15596.py
@@ -1,2 +1,11 @@
+# def solve(a: list) -> int:
+#   return sum(a)
+
+#2024.07.03
+#Another solve
 def solve(a: list) -> int:
-  return sum(a)
+  answer = 0
+  for i in a:
+    answer += i
+
+  return answer
