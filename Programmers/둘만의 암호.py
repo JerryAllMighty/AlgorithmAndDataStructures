ac9e9d2945b68f4011eb61ab5b28c9ff746ba53a
dbsalszz-admin <dbsalszz@naver.com>
2024-07-16 21:10:03 +0900

    [프로그래머스] 둘만의 암호 마무리

--- Programmers/둘만의 암호.py
+++ Programmers/둘만의 암호.py
@@ -2,17 +2,16 @@
 def solution(s, skip, index):
     answer = ''
     # a = 97, z = 122, 전체 26개
-    for i in s:
-        targetIndex = index
-        for j in range(1, index+1):
-            temp = chr(ord(i)+j)
-            if temp in skip:
-                targetIndex += 1
 
-        if ord(i)+ targetIndex > 122:
-            answer += chr(96 + (ord(i)+ targetIndex - 122))
-        else:
-            answer += chr(ord(i)+targetIndex)
+    available = []
+
+    for i in range(97, 123):
+        if chr(i) not in skip:
+            available.append(chr(i))
+
+    for i in s:
+        idx = available.index(i)
+        answer += available[(idx+index)%len(available)]
 
     return answer
 
