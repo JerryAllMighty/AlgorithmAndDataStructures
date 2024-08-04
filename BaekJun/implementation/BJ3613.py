0aef0e705674df7b6009ddec7b2ae3c3eff1b682
KimYoonMin <dbsalszz@naver.com>
2024-08-04 15:05:40 +0900

    [구현] BJ3613 문제풀이 추가

---
+++ BaekJun/implementation/BJ3613.py
@@ -0,0 +1,65 @@
+s = list(input())
+
+
+def isCPlusPlus(targetStringList):
+    result = True
+    for i in range(len(targetStringList)):
+        if targetStringList[i].isupper() is True:
+            return False
+        if i < len(targetStringList) - 1:
+            if targetStringList[i] == '_' and targetStringList[i+1] == '_':
+                return False
+            if i == 0 and targetStringList[i] == '_':
+                return False
+        else:
+            if targetStringList[i] == '_':
+                return False
+
+    return result
+
+
+def isJava(targetStringList):
+    result = True
+    if targetStringList[0].isupper() is True or targetStringList[0] == '_':
+        return False
+
+    for i in range(1, len(targetStringList)):
+        if targetStringList[i] == '_':
+            return False
+
+    return result
+
+
+java = isJava(s)
+cPlusPlus = isCPlusPlus(s)
+
+if java is False and cPlusPlus is False:
+    print('Error!')
+    exit(0)
+
+answer = ''
+skip = False
+
+# Java의 경우
+if java is True:
+    for i in range(len(s)):
+        c = s[i]
+        if c.isupper() is True:
+            answer += '_'
+            answer += c.lower()
+        else:
+            answer += c
+
+elif cPlusPlus is True:
+    for i in range(len(s)):
+        c = s[i]
+        if skip is True:
+            answer += c.upper()
+            skip = False
+            continue
+        if c == '_':
+            skip = True
+        else:
+            answer += c
+
+print(answer)
