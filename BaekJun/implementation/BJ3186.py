5ac048adce19d322ab092d021de6f877fcfa94a9
KimYoonMin <dbsalszz@naver.com>
2024-08-24 13:43:04 +0900

    [구현] BJ3186 문제풀이 추가

--- BaekJun/implementation/BJ3186.py
+++ BaekJun/implementation/BJ3186.py
@@ -1,33 +1,46 @@
-k, l, n = map(int, input().split())
-data = input()
-flushTime = []
-currentTime = 0
-isUse = False
-idx = -1
-lastFlushIdx = n - 1
-while (idx < n):
+k, l, m = map(int, input().split())
+
+lst = input()
+# if len(lst) == 0:
+#     print('NIKAD')
+isUsed = False
+flushLst = []
+lastFlushIdx = m - 1
+idx = 0
+while (idx < m):
+    # 사용중
+    val = lst[idx:idx + k]
+    if lst[idx:idx + k] == '1' * k:
+        isUsed = True
+        for j in range(idx + k, m):
+            if lst[j:j + l] == '0' * l:
+                flushLst.append(j + l)
+                lastFlushIdx = j + l
+                idx = j + l - 1
+                isUsed = False
+                break
     idx += 1
-    if idx <= n - k:
-        if data[idx:idx + k] == '1' * k:
-            isUse = True
-            for j in range(idx + k, n):
-                if data[j:j + l] == '0' * l:
-                    flushTime.append(j + l)
-                    idx = j + l - 1
-                    isUse = False
-                    lastFlushIdx = j + l
-                    break
 
-# 사용중인 것이 남았을 떄
-if isUse is True:
-    cnt = data[lastFlushIdx:].count('0')
-    flushTime.append(n + (l - cnt))
-    idx -= 1
+if isUsed is True:
+    # 연속된 0의 개수가 몇개인지
+    isUnused = False
+    cnt = 0
+    for i in range(lastFlushIdx, m):
+        val = lst[i]
+        if isUnused is True and val == '0':
+            cnt += 1
+        if val == '0':
+            isUnused = True
+            cnt += 1
+        else:
+            cnt = 0
+            isUnused = False
 
-if len(flushTime) == 0:
+    flushLst.append(m + (l - cnt))
+
+if flushLst:
+    flushLst.sort()
+    for i in flushLst:
+        print(i)
+else:
     print('NIKAD')
-    exit(0)
-# 오름차순 정렬
-flushTime.sort()
-for s in flushTime:
-    print(s)
