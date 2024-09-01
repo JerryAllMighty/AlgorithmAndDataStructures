cd58840363c3f3717c60978753f396c99de0d70b
KimYoonMin <dbsalszz@naver.com>
2024-09-01 12:02:58 +0900

    [구현] BJ1972 문제풀이 추가

---
+++ BaekJun/implementation/BJ1972.py
@@ -0,0 +1,31 @@
+def isSurprising(s):
+    result = True
+    d = 0
+    while d <= len(s) - 2:
+        left = 0
+        temp = []
+        while left + d + 2 <= len(s):
+            leftPair = s[left:left + 1]
+            rightPair = s[left + 1 + d:left + 2 + d]
+            if (leftPair, rightPair) in temp:
+                return False
+            else:
+                temp.append((leftPair, rightPair))
+            left += 1
+        d += 1
+
+    return result
+
+
+while True:
+    try:
+        s = input()
+        answer = isSurprising(s)
+        if s == '*':
+            exit(0)
+        if answer is True:
+            print(s + ' is surprising.')
+        else:
+            print(s + ' is NOT surprising.')
+    except EOFError:
+        exit(1)
