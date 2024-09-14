e87d12ef37b5dc02ec63104734d2d0bc222fddc3
KimYoonMin <dbsalszz@naver.com>
2024-09-14 19:49:21 +0900

    [리트코드] 문자열 : :LC 5 문제풀이 추가

--- LeetCode/LC5.py
+++ LeetCode/LC5.py
@@ -1,52 +1,21 @@
-class Solution:
-  def longestPalindrome(s: str) -> str:
-    # Time out
-    # answer = ''
-    # for i in range(len(s)):
-    #   for j in reversed(range(i, len(s))):
-    #     target = s[i:j+1]
-    #     if target == target[::-1]:
-    #       if len(target) > len(answer):
-    #         answer = s[i:j+1]
-    #         break
-    # return answer
-    
-    l1, r1 = 0, 3
-    l2, r2 = 0, 2
-    def isPalindrome(l, r):
-      answer = ''
-      while r <= len(s):
-        target = s[l:r]
-        if target == target[::-1]:
-          l -=1
-          r += 1
-          while l >= 0 and r <= len(s):
-            if s[l:r] == s[l:r][::-1]:
-              target = s[l:r]
-              l -=1
-              r += 1
-            else:
-              break
-            
-          
-          if len(target) > len(answer): 
-            answer = target
+def longestPalindrome(s):
+    left = 0
+    length = len(s)
+    while (length >= 0):
+        targetString = s[left:length]
+        if targetString == targetString[::-1]:
+            return targetString
         else:
-          l += 1
-          r += 1
-          
-      return answer
-          
-    return max(isPalindrome(l1,r1), isPalindrome(l2,r2))
-      
-  
-  
-  print(longestPalindrome('bab'))
-  
-  
-  
-      
-      
-      
-    
-      
+            right = length
+            while (right < len(s)):
+                left += 1
+                right += 1
+                targetString = s[left:right]
+                if targetString == targetString[::-1]:
+                    return targetString
+
+        length -= 1
+        left = 0
+
+
+print(longestPalindrome("cbbd"))
