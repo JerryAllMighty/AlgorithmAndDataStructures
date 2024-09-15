d2fdb75b731ed7524e8d44af3eaca9a1aacb1d08
KimYoonMin <dbsalszz@naver.com>
2024-09-15 14:16:22 +0900

    [구현] BJ2444 문제풀이 추가

--- BaekJun/BJ2444.py
+++ BaekJun/BJ2444.py
@@ -1,33 +1,17 @@
 n = int(input())
 
-# 별의 개수는 2m+1
-# 전체 칸의 개수는 2 * (n-1) + 1
-totalCnt = 2 * n
-emptyList = [' ' for i in range(totalCnt)]
-answers = []
+def recursive2(num):
+    if num == 0:
+        return
+    recursive2(num - 1)
+    if num != n:
+        print(' ' * (n - num) + '*' * (2 * (num) - 1))
 
-for i in range((totalCnt // 2) + 1):
-    # 별 찍기
-    # 별 상단부
-    # 별의 시작점과 갯수만큼 시행
-    startIdx = (totalCnt // 2) - i
-    starCnt = (2 * i) + 1
-    while (starCnt > 0):
-        emptyList[startIdx] = '*'
-        startIdx += 1
-        starCnt -= 1
+def recursive(num):
+    if num == 0:
+        return
+    print(' ' * (n-num) + '*' * (2 *(num) - 1))
+    recursive(num - 1)
 
-    print(''.join(emptyList))
-
-for i in range((totalCnt // 2) + 1, totalCnt):
-    # 별 하단부
-    cnt = i - (totalCnt // 2)
-    leftIdx = 0
-    rightIdx = totalCnt - 1
-    while (cnt > 0):
-        emptyList[leftIdx] = ' '
-        emptyList[rightIdx] = ' '
-        cnt -= 1
-        leftIdx += 1
-        rightIdx -= 1
-    print(''.join(emptyList))
+recursive2(n)
+recursive(n)
