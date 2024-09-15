dd3636d530484a6e3c220ab46a32601e05b98765
KimYoonMin <dbsalszz@naver.com>
2024-09-15 21:21:27 +0900

    [구현] BJ14569 비트마스킹 문제 풀이 추가

--- BaekJun/implementation/BJ14569.py
+++ BaekJun/implementation/BJ14569.py
@@ -1,26 +1,62 @@
-n = int(input())
-subjects = []
-for _ in range(n):
-    subjects.append(input().split())
+# n = int(input())
+# subjects = []
+# for _ in range(n):
+#     subjects.append(input().split())
+#
+# m = int(input())
+#
+# answers = []
+# for _ in range(m):
+#     answer = 0
+#     availble = input().split()
+#     for i in subjects:
+#         availbleLst = [availble[i] for i in range(1, len(availble))]
+#         subjectsLst = [i[j] for j in range(1,len(i))]
+#         bFlag = True
+#         for j in subjectsLst:
+#             if j not in availbleLst:
+#                 bFlag = False
+#                 break
+#
+#         if bFlag is True:
+#             answer += 1
+#     answers.append(answer)
+#
+# for j in answers:
+#     print(j)
 
-m = int(input())
 
-answers = []
-for _ in range(m):
-    answer = 0
-    availble = input().split()
-    for i in subjects:
-        availbleLst = [availble[i] for i in range(1, len(availble))]
-        subjectsLst = [i[j] for j in range(1,len(i))]
-        bFlag = True
-        for j in subjectsLst:
-            if j not in availbleLst:
-                bFlag = False
-                break
+import sys
 
-        if bFlag is True:
-            answer += 1
-    answers.append(answer)
 
-for j in answers:
-    print(j)
+def get_binary(arr):
+    cur = 0
+    for num in arr:
+        cur |= (1 << num - 1)
+    return cur
+
+
+def main():
+    n = int(sys.stdin.readline())
+    choices = []
+    for _ in range(n):
+        temp = list(map(int, sys.stdin.readline().split()))
+        choices.append(get_binary(temp[1:]))
+
+    m = int(sys.stdin.readline())
+    ans = []
+    for _ in range(m):
+        temp = list(map(int, sys.stdin.readline().split()))
+        cur = get_binary(temp[1:])
+        count = 0
+        for bitmask in choices:
+            if cur & bitmask == bitmask:
+                count += 1
+        ans.append(count)
+
+    for c in ans:
+        print(c)
+
+
+if __name__ == "__main__":
+    main()
