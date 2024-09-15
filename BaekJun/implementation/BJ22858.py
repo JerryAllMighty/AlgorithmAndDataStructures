0d2edf935e5c7dea56335b380c8421f0ff2e7e7c
KimYoonMin <dbsalszz@naver.com>
2024-09-15 14:37:16 +0900

    [구현] BJ22858 문제 풀이 추가

--- BaekJun/implementation/BJ22858.py
+++ BaekJun/implementation/BJ22858.py
@@ -1,18 +1,16 @@
 n, k = map(int, input().split())
 
-result = list(map(int, input().split()))
-order = list(map(int, input().split()))
-cmp = [chr(97 + i) for i in range(n)]
-init = [i for i in cmp]
-answer = []
-for i in range(k):
-    temp = [i for i in cmp]
-    for j in range(len(order)):
-        cmp[j] = temp[order[j]-1]
-for j in init:
-    for i in range(len(cmp)):
-        if j == cmp[i]:
-            answer.append(result[i])
-            continue
+finalLst = list(map(int, input().split()))
+shuffleOrder = list(map(int, input().split()))
+answers = [chr(97+i) for i in range(n)]
+lst = [i for i in answers]
+for _ in range(k):
+    temp = [i for i in lst]
+    for i in range(n):
+        lst[i] = temp[shuffleOrder[i]-1]
 
-print(*answer)
+for j in answers:
+    idx = lst.index(j)
+    print(finalLst[idx],end=' ')
+
+
