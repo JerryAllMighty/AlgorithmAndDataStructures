f8a72d3e4e37ce4e7e3a74741cef810330df5520
KimYoonMin <dbsalszz@naver.com>
2024-09-15 15:00:12 +0900

    [구현] BJ22858 사이클 활용 개념 예시

--- BaekJun/implementation/BJ22858.py
+++ BaekJun/implementation/BJ22858.py
@@ -1,16 +1,39 @@
-n, k = map(int, input().split())
+# # 32140	2428
+# n, k = map(int, input().split())
+#
+# finalLst = list(map(int, input().split()))
+# shuffleOrder = list(map(int, input().split()))
+# answers = [chr(97+i) for i in range(n)]
+# lst = [i for i in answers]
+# for _ in range(k):
+#     temp = [i for i in lst]
+#     for i in range(n):
+#         lst[i] = temp[shuffleOrder[i]-1]
+#
+# for j in answers:
+#     idx = lst.index(j)
+#     print(finalLst[idx],end=' ')
 
-finalLst = list(map(int, input().split()))
-shuffleOrder = list(map(int, input().split()))
-answers = [chr(97+i) for i in range(n)]
-lst = [i for i in answers]
-for _ in range(k):
-    temp = [i for i in lst]
-    for i in range(n):
-        lst[i] = temp[shuffleOrder[i]-1]
 
-for j in answers:
-    idx = lst.index(j)
-    print(finalLst[idx],end=' ')
+import sys
 
+input = lambda: sys.stdin.readline().rstrip()
+g = lambda: [*map(int, input().split())]
 
+N, K = g()
+S = g()
+D = [*map(lambda x: int(x) - 1, input().split())]
+ans = [0] * N
+for i in range(N):
+    if ans[i]:
+        continue
+    cycle = [i]
+    now = i
+    while D[now] != i:
+        now = D[now]
+        cycle.append(now)
+    for idx in range(len(cycle)):
+        from_idx = cycle[idx]
+        to_idx = cycle[(idx + K) % len(cycle)]
+        ans[to_idx] = S[from_idx]
+print(*ans)
