5789c0a0dd24f1c05fb7b83b04863837865ce87f
KimYoonMin <dbsalszz@naver.com>
2024-10-12 13:41:50 +0900

    [구현] BJ17499 시간초과는 해결, 조건 분기 미해결

--- BaekJun/implementation/BJ17499.py
+++ BaekJun/implementation/BJ17499.py
@@ -1,20 +1,17 @@
 size, cnt = map(int, input().split())
 lst = list(map(int, input().split()))
 
-orders = [i for i in range(size)]
 for _ in range(cnt):
     rules = list(map(int, input().split()))
     temp = [0 for _ in range(size)]
     if rules[0] == 1:
         lst[rules[1]-1] += rules[2]
     elif rules[0] == 2:
-        for i in range(size):
-            idx = (i + rules[1]) % size
-            orders[i] = idx
+        # 23401
+        gap = rules[1] - 1
+        lst = [lst[(i + gap) % size] for i in range(size)]
     elif rules[0] == 3:
-        for i in range(size):
-            idx = (i - rules[1]) % size
-            orders[i] = idx
-
-answer = [lst[i] for i in orders]
-print(*answer)
+        # 1234 0
+        gap = rules[1]
+        lst = [lst[(i + gap) % size] for i in range(size)]
+print(*lst)
