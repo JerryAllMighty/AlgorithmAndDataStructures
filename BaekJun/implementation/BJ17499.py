12704000e54f9a7960728ee8d18139d4435aaec4
KimYoonMin <dbsalszz@naver.com>
2024-10-26 20:31:27 +0900

    [구현] BJ17499 문제풀이 변경, 시간초과 2

--- BaekJun/implementation/BJ17499.py
+++ BaekJun/implementation/BJ17499.py
@@ -1,17 +1,23 @@
 size, cnt = map(int, input().split())
 lst = list(map(int, input().split()))
 
+gap = 0
 for _ in range(cnt):
     rules = list(map(int, input().split()))
     temp = [0 for _ in range(size)]
     if rules[0] == 1:
-        lst[rules[1]-1] += rules[2]
+        aa = (rules[1]-1-gap)%size
+        lst[aa] += rules[2]
     elif rules[0] == 2:
-        # 23401
-        gap = rules[1] - 1
-        lst = [lst[(i + gap) % size] for i in range(size)]
+        gap += rules[1]
+        # lst = [lst[(i + gap) % size] for i in range(size)]
     elif rules[0] == 3:
-        # 1234 0
-        gap = rules[1]
-        lst = [lst[(i + gap) % size] for i in range(size)]
-print(*lst)
+        gap -= rules[1]
+        # lst = [lst[(i + gap) % size] for i in range(size)]
+# 01234
+# 23401
+answer = [0 for _ in range(size)]
+for i in range(size):
+    aa = (i+gap)%size
+    answer[aa] = lst[i]
+print(*answer)
