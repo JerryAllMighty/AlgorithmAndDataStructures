4fe5418900bbb30f4cee1b1de2876dba2ddb6e23
KimYoonMin <dbsalszz@naver.com>
2025-01-04 23:36:57 +0900

    [문자열] BJ1919 추가

---
+++ BaekJun/String/BJ1919.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Main {
+    public static List<Integer> isExist = new ArrayList<>();
+    public static void containsCharThenRemove(char c, char[] charList) {
+        for (int i = 0; i < charList.length; i++) {
+            char eachChar = charList[i];
+            if (eachChar == c && !isExist.contains(i)) {
+                isExist.add(i);
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] a = sc.nextLine().toCharArray();
+        char[] b = sc.nextLine().toCharArray();
+        for (int i = 0; i < a.length; i++) {
+            char c = a[i];
+            containsCharThenRemove(c, b);
+        }
+        System.out.println((b.length - isExist.size()) + (a.length - isExist.size()));
+
+    }
+}
