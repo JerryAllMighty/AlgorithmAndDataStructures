98f52477c0245492c85d1a31fa74d5e6cb723980
KimYoonMin <dbsalszz@naver.com>
2025-01-04 23:36:28 +0900

    [문자열] BJ 1157 문제풀이 추가

---
+++ BaekJun/String/BJ1157.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] input = sc.nextLine().toLowerCase().toCharArray();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < input.length; i++) {
+            char c = input[i];
+            if (map.get(c) == null) {
+                map.put(c, 1);
+            } else {
+                int val = map.get(c);
+                map.put(c, val + 1);
+            }
+
+        }
+        int maxNum = 0;
+        String maxKey = "";
+        for (Character key : map.keySet()) {
+            int val = map.get(key);
+            if (val >= maxNum) {
+                maxNum = val;
+                maxKey = key.toString();
+            }
+        }
+        for (Character key : map.keySet()) {
+            int val = map.get(key);
+            if (val == maxNum && !key.toString().equals(maxKey)) {
+                maxKey = "?";
+                break;
+            }
+        }
+
+        System.out.println(maxKey.toUpperCase());
+
+    }
+}
