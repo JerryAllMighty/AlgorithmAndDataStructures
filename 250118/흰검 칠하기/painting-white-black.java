53334a1159d06a8e3c6dd9b81efc051ca926aef0
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2025-01-18 16:27:31 +0900

    Title: 흰검 칠하기, Time: 701ms, Memory: 48MB, Status: Passed - CodeTree

--- 250118/흰검 칠하기/painting-white-black.java
+++ 250118/흰검 칠하기/painting-white-black.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-
         int current = 0;
+        String previousDirection = "";
 
         for (int i = 0; i < n; i++) {
             String[] input = sc.nextLine().split(" ");
@@ -20,25 +20,25 @@ public class Main {
                 if (val == null) {
                     val = new ArrayList<>();
                 }
-
-                if (direction.equals("R")) {
-                    val.add("B");
-                } else if (direction.equals("L")) {
-                    val.add("W");
+                if (!val.contains("G")) {
+                    if (direction.equals("R")) {
+                        val.add("B");
+                    } else if (direction.equals("L")) {
+                        val.add("W");
+                    }
+                    int blackCnt = (int) val.stream().filter(x -> x.equals("B")).count();
+                    int whiteCnt = (int) val.stream().filter(x -> x.equals("W")).count();
+                    if (blackCnt >= 2 && whiteCnt >= 2) {
+                        val.remove(val.size() - 1);
+                        val.add("G");
+                    }
+                    lst.put(current, val);
                 }
-                int blackCnt = (int) val.stream().filter(x -> x.equals("B")).count();
-                int whiteCnt = (int) val.stream().filter(x -> x.equals("W")).count();
-                if (blackCnt >= 2 && whiteCnt >= 2) {
-                    val.remove(val.size() - 1);
-                    val.add("G");
-                }
-                lst.put(current, val);
                 if (direction.equals("R") && j != cnt - 1) {
                     current++;
                 } else if (direction.equals("L") && j != cnt - 1) {
                     current--;
                 }
-
             }
         }
         long whiteCnt = lst.values().stream().filter(x -> x != null && x.size() > 0 && x.get(x.size() - 1).equals("W")).count();
