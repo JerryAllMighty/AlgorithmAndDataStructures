4e0993fbc2fcb75f36b99931cc0ff1553e2e440a
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2025-01-18 21:34:04 +0900

    Title: 사각형의 총 넓이 2, Time: 315ms, Memory: 17MB, Status: Passed - CodeTree

--- 250118/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 250118/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
 public class Main {
+    static int OFFSET = 100;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        int[][] lst = new int[100][100];
+        int[][] lst = new int[201][201];
 
         for (int i = 0; i < n; i++) {
             int[] input = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
@@ -16,10 +17,8 @@ public class Main {
 
             for (int j = x1; j < x2; j++) {
                 for (int k = y1; k < y2; k++) {
-                    int val = lst[j][k];
-                    if (val == 0) {
-                        lst[j][k] = 1;
-                    }
+                    lst[j + OFFSET][k + OFFSET]++;
+
                 }
             }
         }
