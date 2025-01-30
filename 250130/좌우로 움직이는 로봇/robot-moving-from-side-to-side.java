68f91279e1e88322fdf4688e2e2cf19ade6632b7
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2025-01-30 14:46:07 +0900

    Title: 좌우로 움직이는 로봇, Time: 693ms, Memory: 66MB, Status: Runtime Error - CodeTree

---
+++ 250130/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] input = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = input[0];
+        int m = input[1];
+        int[] lstA = new int[1000000];
+        int[] lstB = new int[1000000];
+
+        int previousIdxA = 0;
+        int previousIdxB = 0;
+
+        int previousTimeA = 0;
+        int previousTimeB = 0;
+
+
+        for (int i = 0; i < n; i++) {
+            String[] directionCnt = sc.nextLine().split(" ");
+            int cnt = Integer.parseInt(directionCnt[0]);
+            String direction = directionCnt[1];
+
+            for (int j = 1; j <= cnt; j++) {
+                if (direction.equals("L")) {
+                    lstA[previousTimeA + j] = previousIdxA - 1;
+                    previousIdxA--;
+                }
+                if (direction.equals("R")) {
+                    lstA[previousTimeA + j] = previousIdxA + 1;
+                    previousIdxA++;
+                }
+
+            }
+            previousTimeA += cnt;
+        }
+        Arrays.fill(lstA, previousTimeA + 1, 1000000, previousIdxA);
+
+        for (int i = 0; i < m; i++) {
+            String[] directionCnt = sc.nextLine().split(" ");
+            int cnt = Integer.parseInt(directionCnt[0]);
+            String direction = directionCnt[1];
+
+            for (int j = 1; j <= cnt; j++) {
+                if (direction.equals("L")) {
+                    lstB[previousTimeB + j] = previousIdxB - 1;
+                    previousIdxB--;
+                }
+                if (direction.equals("R")) {
+                    lstB[previousTimeB + j] = previousIdxB + 1;
+                    previousIdxB++;
+                }
+
+            }
+            previousTimeB += cnt;
+        }
+        Arrays.fill(lstB, previousTimeB + 1, 1000000, previousIdxB);
+
+        int answer = 0;
+        for (int i = 1; i < Math.max(previousTimeA, previousTimeB); i++) {
+            if ((lstA[i - 1] != lstB[i - 1]) && (lstA[i] == lstB[i])) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
+
+
+
+
+
+
