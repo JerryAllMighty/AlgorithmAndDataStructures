811414db7d3650a728617e0c5c31eaa0fe7d8b72
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2025-01-30 22:06:15 +0900

    Title: 되돌아오기, Time: 103ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250130/되돌아오기/come-back.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main {
+    static int[] dx = {0, 1, -1, 0};
+    static int[] dy = {-1, 0, 0, 1};
+
+    static int getDir(String d) {
+        if (d.equals("W")) {
+            return 0;
+        } else if (d.equals("S")) {
+            return 1;
+        } else if (d.equals("N")) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int answer = -1;
+        int x = 0;
+        int y = 0;
+        int time = 0;
+        for (int i = 0; i < n; i++) {
+            String[] input = sc.nextLine().split(" ");
+            String direction = input[0];
+            int cnt = Integer.parseInt(input[1]);
+            int d = getDir(direction);
+            for (int j = 0; j < cnt; j++) {
+                x += dx[d];
+                y += dy[d];
+                time++;
+                if(x == 0 && y == 0){
+                    answer = time;
+                }
+            }
+
+
+        }
+        System.out.println(answer);
+
+    }
+}
+
+
+
+
+
+
+
+
