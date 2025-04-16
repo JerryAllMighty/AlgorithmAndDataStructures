c22b3ce5eaea40498b0bb5959aa36d0ce65d06ad
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2025-04-16 21:18:19 +0900

    Title: 아름다운 수열 2, Time: 304ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250416/아름다운 수열 2/beautiful-sequence-2.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] input = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = input[0];
+        int m = input[1];
+
+        int[] lstA = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] lstB = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int answer = 0;
+        for (int i = 0; i <= n - m; i++) {
+            int[] temp = new int[m];
+            Arrays.fill(temp, 0);
+            for (int j = 0; j < m; j++) {
+                int val = lstA[i + j];
+                for (int k = 0; k < lstB.length; k++) {
+                    if (val == lstB[k]) {
+                        temp[j] = val;
+                    }
+
+                }
+
+            }
+            //아름다운 수인지 판단
+            long cnt = Arrays.stream(temp).filter(x -> x != 0).count();
+            if (cnt == m) {
+                answer++;
+            }
+
+
+        }
+        System.out.println(answer);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
