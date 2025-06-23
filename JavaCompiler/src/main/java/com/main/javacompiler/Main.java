f5c5783374b82424a57f87f518be39dc3d85f40e
KimYoonMin <dbsalszz@naver.com>
2025-06-23 11:19:54 +0900

    파일 구조 재정립, java compile 용 클래스 추가

--- JavaCompiler/src/main/java/com/main/javacompiler/Main.java
+++ JavaCompiler/src/main/java/com/main/javacompiler/Main.java
@@ -1,7 +1,11 @@
 package com.main.javacompiler;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(10);
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        System.out.println(n);
     }
 }
