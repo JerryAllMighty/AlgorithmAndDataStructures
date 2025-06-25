7cb219705bd4e16369aab31af136b9adc260ba38
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2025-06-25 12:12:45 +0900

    Title: 황금비율 토스트, Time: 326ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250625/황금비율 토스트/golden-toast.java
@@ -0,0 +1,54 @@
+
+import java.util.*;
+
+public class Main {
+    static ListIterator<String> it;
+
+    static void fn(String[] type) {
+        String dir = type[0];
+        String chr = "";
+        if (type.length > 1) {
+            chr = type[1];
+        }
+
+        if (dir.equals("L")) {
+            if (it.hasPrevious()) {
+                it.previous();
+            }
+
+        } else if (dir.equals("R")) {
+            if (it.hasNext()) {
+                it.next();
+            }
+        } else if (dir.equals("D")) {
+            it.next();
+            it.remove();
+
+        } else if (dir.equals("P") && !chr.isEmpty()) {
+            it.add(chr);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] cntList = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = cntList[0];
+        int m = cntList[1];
+
+        String encryptMsg = sc.nextLine();
+        List<String> lst = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            lst.add(encryptMsg.substring(i, i + 1));
+        }
+
+        it = lst.listIterator(n);
+        for (int i = 0; i < m; i++) {
+            String[] info = sc.nextLine().split(" ");
+            fn(info);
+        }
+
+        lst.forEach(x -> System.out.print(x + ""));
+
+
+    }
+}
