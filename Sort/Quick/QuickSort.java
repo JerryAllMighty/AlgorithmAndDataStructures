300c4badef189c9b6a11b0b59f6d7e2daf8c30b2
KimYoonMin <dbsalszz@naver.com>
2025-07-12 19:52:22 +0900

    [퀵정렬] 기본 구현 코드 추가

---
+++ Sort/Quick/QuickSort.java
@@ -0,0 +1,31 @@
+package Sort.Quick;
+
+public class QuickSort {
+//    function partition(arr[], low, high)
+//    set pivot = select_pivot(arr, low, high)
+//    set i = low - 1
+//
+//  for j = low ... j <= high - 1
+//            if arr[j] < pivot
+//    i += 1
+//    swap (arr[i], arr[j])
+//
+//    swap (arr[i + 1], arr[high])
+//  return i + 1
+//
+//    function quick_sort(arr[], low, high)
+//  if low < high
+//            pos = partition(arr, low, high)
+//
+//    quick_sort(arr, low, pos - 1)
+//    quick_sort(arr, pos + 1, high)
+
+}
+
+//function quick_sort(arr[], low, high)
+//  if low < high                     // 원소의 개수가 2개 이상인 경우에만 진행
+//        pos = partition(arr, low, high) // pivot 기준으로 좌우로 분할 한 후
+//// 해당 pivot의 위치를 pos에 넣어줍니다.
+//quick_sort(arr, low, pos - 1)   // pivot의 왼쪽 구간에 있는 원소들을 정렬합니다.
+//quick_sort(arr, pos + 1, high)  // pivot의 오른쪽 구간에 있는 원소들을 정렬합니다.
+
