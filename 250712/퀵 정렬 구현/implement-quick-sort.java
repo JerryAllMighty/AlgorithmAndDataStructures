0831e1ef82848577a49dbe40e7c12023a35ea0bb
JerryTheSWEngineer <66665210+JerryAllMighty@users.noreply.github.com>
2025-07-12 20:49:46 +0900

    Title: 퀵 정렬 구현, Time: 3083ms, Memory: 21MB, Status: Time Limit Exceed - Codetree

---
+++ 250712/퀵 정렬 구현/implement-quick-sort.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    static int[] arr;
+
+    static int partition(int startIdx, int endIdx) {
+        int pivot = arr[endIdx];
+        int i = startIdx - 1;
+        for (int j = startIdx; j < endIdx; j++) {
+            if (arr[j] < pivot) {
+                i += 1;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = pivot;
+        arr[endIdx] = temp;
+        return i + 1;
+    }
+
+    static void quickSort(int startIdx, int endIdx) {
+        if (startIdx < endIdx) {
+            int pos = partition(0, endIdx);
+            quickSort(0, pos - 1);
+            quickSort(pos + 1, endIdx);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        //quick sort
+        quickSort(0, n - 1);
+        for(int i =0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
