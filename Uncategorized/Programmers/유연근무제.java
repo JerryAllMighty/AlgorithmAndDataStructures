8dde224653580dfff972444feaad2ef2a38716cb
KimYoonMin <dbsalszz@naver.com>
2025-09-07 22:01:52 +0900

    [프로그래머스] 유연근무제 정답 추가

--- Uncategorized/Programmers/유연근무제.java
+++ Uncategorized/Programmers/유연근무제.java
@@ -2,12 +2,14 @@ package Uncategorized.Programmers;
 
 public class 유연근무제 {
 
+    //TODO : https://school.programmers.co.kr/learn/courses/30/lessons/388351?language=java >> 에서 틀린 테스트 케이스 정리하기
     public static int solution(int[] schedules, int[][] timelogs, int startday) {
         int answer = schedules.length;
         int n = schedules.length;
         for (int i = 0; i < n; i++) {
             int[] timelog = timelogs[i];
             int timeLimit = schedules[i] + 10;
+            //10분 더한 시간을 날짜형식에 맟추어서 변환
             if ((timeLimit % 100) >= 60) {
                 int hour = (((timeLimit/100) + 1) *  100);
                 int minute = (timeLimit % 100) % 60;
