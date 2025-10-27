ee6c4978da6d96a7c9a935a19a333db3354b076f
KimYoonMin <dbsalszz@naver.com>
2025-10-27 15:05:24 +0900

    [String] LC205 문제 풀이 초안

---
+++ Implementation/String/LC205.java
@@ -0,0 +1,35 @@
+package Implementation.String;
+
+public class LC205 {
+}
+
+//2025.10.27 오답 / 다시 문제 풀이 필요, 초안
+//package com.main.javacompile;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//
+//public class Main {
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        String s = br.readLine();
+//        String t = br.readLine();
+//
+//        char temp = (char)1;
+//        for (int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//            s = s.replace(c, temp);
+//
+//            char ch = t.charAt(i);
+//            t = t.replace(ch, temp);
+//
+//            temp = (char)((int)temp + 1);
+//        }
+//        System.out.println(s.equals(t));
+//    }
+//}
+
+
