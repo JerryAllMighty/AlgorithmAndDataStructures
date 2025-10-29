75345fa5e4088922bf6d23fc6d35318e52e1ca1b
KimYoonMin <dbsalszz@naver.com>
2025-10-29 12:00:41 +0900

    [String] LC290 다른 사람 풀이 추가

--- Implementation/String/LC290.java
+++ Implementation/String/LC290.java
@@ -49,3 +49,37 @@ public class LC290 {
 
     }
 }
+
+//2025.10.29 다른 사람 풀이 추가
+//import java.util.HashMap;
+//import java.util.Objects;
+//
+//class Solution {
+//    public boolean wordPattern(String pattern, String s) {
+//        String[] words = s.split(" ");
+//        if (words.length != pattern.length()) {
+//            return false;
+//        }
+//
+//        HashMap<Character, String> map = new HashMap<>();
+//
+//        for (int i = 0; i < pattern.length(); i++) {
+//            char ch = pattern.charAt(i);
+//            String word = words[i];
+//
+//            if (map.containsKey(ch)) {
+//                // If mapping for char already exists, check consistency
+//                if (!map.get(ch).equals(word)) {
+//                    return false;
+//                }
+//            } else {
+//                // If mapping for char doesn't exist, check if word is already mapped to another char
+//                if (map.containsValue(word)) {
+//                    return false;
+//                }
+//                // Establish new mapping
+//                map.put(ch, word);
+//            }
+//        }
+//        return true;
+//    }
