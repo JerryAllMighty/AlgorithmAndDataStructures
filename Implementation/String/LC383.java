acd588efd72585fdd9dd2b2f824a2c6f806936c7
KimYoonMin <dbsalszz@naver.com>
2025-10-29 11:32:25 +0900

    [String] LC383 다른 사람 풀이 추가

--- Implementation/String/LC383.java
+++ Implementation/String/LC383.java
@@ -33,3 +33,20 @@ public class LC383 {
         System.out.println(true);
     }
 }
+
+//2025.10.29 다른 사람 풀이 추가
+//class Solution {
+//    public boolean canConstruct(String ransomNote, String magazine) {
+//        // Initialize an array of count with the size 26...
+//        int[] counter = new int[128];
+//        // Traverse a loop through the entire String of magazine where char ch stores the char at the index of magazine...
+//        for (final char ch : magazine.toCharArray())
+//            ++counter[ch];
+//        // Run another for loop for ransomNote...
+//        for (final char ch : ransomNote.toCharArray())
+//            // If the charachter doesn't exists in magazine for ransomNote, we return false...
+//            if (--counter[ch] < 0)
+//                return false;
+//        return true;      // If nothing goes wrong, return true...
+//    }
+//}
