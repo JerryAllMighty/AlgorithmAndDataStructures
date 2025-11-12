36eaa2c6a3305e61fd9582bd5ed47a9afba5e546
KimYoonMin <dbsalszz@naver.com>
2025-11-12 16:01:41 +0900

    [BFS] BJ2606 풀이 추가 (정답 풀이, 다른 풀이도 추가)

--- Search/DFS/BJ2606.java
+++ Search/DFS/BJ2606.java
@@ -43,4 +43,61 @@ public class BJ2606 {
         bw.close();
         br.close();
     }
+    /* 2025.11.12 BFS 풀이도 추가   
+
+    static int[][] graph;
+    static boolean[] visited;
+    static int answer;
+    static Queue<Integer> q;
+
+    static void dfs(int node) {
+        visited[node] = true;
+        answer++;
+        for (int i = 1; i < graph.length; i++) {
+            if (graph[node][i] == 1 && !visited[i]) {
+                bfs(i);
+            }
+        }
+    }
+
+    static void bfs(int node) {
+        q = new ArrayDeque<>();
+        q.add(node);
+        visited[node] = true;
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            answer++;
+            for (int i = 1; i < graph.length; i++) {
+                if (graph[now][i] == 1 && !visited[i]) {
+                    q.add(i);
+                    visited[i] = true;
+                }
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        graph = new int[n + 1][n + 1];
+        visited = new boolean[n + 1];
+        for (int i = 0; i < m; i++) {
+            int[] pair = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            graph[pair[0]][pair[1]] = 1;
+            graph[pair[1]][pair[0]] = 1;
+        }
+//        dfs(1);
+        bfs(1);
+        bw.write(Integer.toString(answer - 1));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+     */
 }
