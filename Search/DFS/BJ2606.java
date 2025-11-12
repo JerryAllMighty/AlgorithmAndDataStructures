400fbf88c3c4e0d97b20c3d1a0f14cc948f64025
KimYoonMin <dbsalszz@naver.com>
2025-11-12 15:12:56 +0900

    [DFS] BJ2606 풀이 추가 (개인 풀이, 통과 완료)

---
+++ Search/DFS/BJ2606.java
@@ -0,0 +1,46 @@
+package Search.DFS;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class BJ2606 {
+
+    static int[][] graph;
+    static boolean[] visited;
+
+    static void bfs(int start) {
+        visited[start] = true;
+        for (int i = 1; i < graph.length; i++) {
+            if (graph[start][i] == 1 && !visited[i]) {
+                bfs(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        graph = new int[n + 1][n + 1];
+        visited = new boolean[n + 1];
+        for (int i = 0; i < m; i++) {
+            int[] pair = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            graph[pair[0]][pair[1]] = 1;
+            graph[pair[1]][pair[0]] = 1;
+        }
+        bfs(1);
+        int answer = 0;
+        for (int i = 2; i < visited.length; i++) {
+            if (visited[i]) {
+                answer++;
+            }
+        }
+        bw.write(Integer.toString(answer));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
