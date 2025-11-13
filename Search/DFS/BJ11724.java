689aa3d2c2bac85d190f2c7f585b7133d29aa62c
KimYoonMin <dbsalszz@naver.com>
2025-11-13 10:25:51 +0900

    [DFS] BJ11724 풀이 추가 (정답풀이, 통과 완료)

--- Search/DFS/BJ11724.java
+++ Search/DFS/BJ11724.java
@@ -8,30 +8,12 @@ public class BJ11724 {
     static int[][] graph;
     static boolean[] visited;
 
-    static int isExistThenWhichNode() {
-        for (int i = 0; i < graph.length; i++) {
-            for (int val : graph[i]) {
-                if (val == 1) {
-                    return i;
-                }
-            }
-        }
-        return -1;
-    }
-
     static void dfs(int node) {
         visited[node] = true;
         for (int i = 1; i < graph.length; i++) {
-            if (graph[node][i] != 0) {
-                graph[node][i]++;
-                graph[i][node]++;
-                if (!visited[i]) {
-                    dfs(i);
-                }
-
-
+            if (graph[node][i] != 0 && !visited[i]) {
+                dfs(i);
             }
-
         }
     }
 
@@ -44,20 +26,17 @@ public class BJ11724 {
         int m = input[1];
         graph = new int[n + 1][n + 1];
         visited = new boolean[n + 1];
-        int nextNode = 1;
         for (int i = 0; i < m; i++) {
             int[] nodes = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             graph[nodes[0]][nodes[1]] = 1;
             graph[nodes[1]][nodes[0]] = 1;
-            if (i == 0) {
-                nextNode = nodes[0];
-            }
         }
         int answer = 0;
-        while (nextNode > -1) {
-            dfs(nextNode);
-            answer++;
-            nextNode = isExistThenWhichNode();
+        for (int i = 1; i < n + 1; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                answer++;
+            }
         }
 
         bw.write(Integer.toString(answer));
