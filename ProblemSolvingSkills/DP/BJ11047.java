ce243ff68d42f952a8790ecd33f06b7d570dda28
KimYoonMin <dbsalszz@naver.com>
2025-11-17 12:22:07 +0900

    [DP] BJ11047 풀이 추가 (정답 코드 추가)

--- ProblemSolvingSkills/DP/BJ11047.java
+++ ProblemSolvingSkills/DP/BJ11047.java
@@ -36,3 +36,34 @@ public class BJ11047 {
         br.close();
     }
 }
+/*
+2025.11.17 정답 코드 추가
+public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = input[0];
+        int k = input[1];
+        int[] values = new int[n];
+        for (int i = 0; i < n; i++) {
+            int value = Integer.parseInt(br.readLine());
+            values[i] = value;
+        }
+
+        int answer = 0;
+        int compareIndex = n - 1;
+        int moneyLeft = k;
+        for (int i = n - 1; i >= 0; i--) {
+            if (k >= values[i]) {
+                answer += moneyLeft / values[i];
+                moneyLeft %= values[i];
+            }
+
+        }
+        bw.write(Integer.toString(answer));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+ */
