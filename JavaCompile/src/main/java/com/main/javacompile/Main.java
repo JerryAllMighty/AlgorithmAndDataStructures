ebeab3ddcfbdc878f31b5549cea343d611f79697
KimYoonMin <dbsalszz@naver.com>
2025-11-23 21:20:24 +0900

    main 기본 셋팅 설정

--- JavaCompile/src/main/java/com/main/javacompile/Main.java
+++ JavaCompile/src/main/java/com/main/javacompile/Main.java
@@ -1,21 +1,29 @@
 package com.main.javacompile;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        for(int n : arr){
-            System.out.print(n + " ");
-        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-
+        bw.flush();
+        bw.close();
+        br.close();
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
