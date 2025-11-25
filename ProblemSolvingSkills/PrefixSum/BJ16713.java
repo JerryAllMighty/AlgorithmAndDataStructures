b33662bb2313cbd745750618e86b5bea90048874
KimYoonMin <dbsalszz@naver.com>
2025-11-25 12:26:25 +0900

    [PrefixSum] BJ16713 (개인 문제 풀이, 통과 못 함)

--- ProblemSolvingSkills/PrefixSum/BJ16713.java
+++ ProblemSolvingSkills/PrefixSum/BJ16713.java
@@ -4,32 +4,28 @@ import java.io.*;
 import java.util.Arrays;
 
 public class BJ16713 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int[] info = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int n = info[0];
         int q = info[1];
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] prefixSumArr = arr.clone();
-        prefixSumArr[0] = 0;
-        for (int i = 1; i < n; i++) {
-            prefixSumArr[i] = prefixSumArr[i] == prefixSumArr[i - 1] ? 0 : 1;
+        int[] arr = new int[n + 1];
+        int[] temp = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int i = 1; i <= n; i++) {
+            arr[i] = temp[i - 1];
         }
-        int[] answers = new int[q];
+        int[] prefixSumArr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            prefixSumArr[i] = arr[i] ^ prefixSumArr[i - 1];
+        }
+        int answer = 0;
         for (int i = 0; i < q; i++) {
             int[] queries = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int j = queries[1] - 1;
-            int k = queries[0] - 2;
-            if (k >= 0) {
-                answers[i] = prefixSumArr[j] == prefixSumArr[k] ? 0 : 1;
-            } else {
-                answers[i] = prefixSumArr[j];
-            }
-        }
-        int answer = answers[0];
-        for (int i = 1; i < q; i++) {
-            answer = answers[i] == answers[i - 1] ? 0 : 1;
+            int j = queries[1];
+            int k = queries[0];
+            answer = prefixSumArr[j] ^ prefixSumArr[k - 1];
         }
         bw.write(String.valueOf(answer));
         bw.flush();
