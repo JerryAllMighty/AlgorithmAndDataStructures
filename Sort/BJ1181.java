eb3effc214b9e14dd65b13a66950133190a5035c
KimYoonMin <dbsalszz@naver.com>
2025-11-27 21:01:21 +0900

    [Sort] BJ1181 (개인 풀이, 통과 완료, LinkedHashSet 사용)

--- Sort/BJ1181.java
+++ Sort/BJ1181.java
@@ -39,3 +39,57 @@ public class BJ1181 {
         br.close();
     }
 }
+
+/*
+ * 다른 풀이, LinkedHashSet 사용
+ * package com.main.javacompile;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] words = new String[n];
+        for (int i = 0; i < n; i++) {
+            String word = br.readLine();
+            words[i] = word;
+        }
+        Arrays.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                }
+                return o1.length() - o2.length();
+            }
+        });
+        LinkedHashSet<String> set2 = new LinkedHashSet(List.of(words));
+        for (String s : set2) {
+            bw.write(s + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
