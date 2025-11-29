55489acdf67f2d8c93dbbf7572efa886a4415aba
KimYoonMin <dbsalszz@naver.com>
2025-11-29 21:13:01 +0900

    [Sort] BJ10814 문제 풀이 추가 (개인 문제 풀이, 통과 완료)

---
+++ Sort/BJ10814.java
@@ -0,0 +1,34 @@
+package Sort;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BJ10814 {
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            int n = Integer.parseInt(br.readLine());
+            String[][] arr = new String[n][];
+            for (int i = 0; i < n; i++) {
+                String[] info = br.readLine().split(" ");
+                arr[i] = info;
+            }
+            Arrays.sort(arr, new Comparator<String[]>() {
+                @Override
+                public int compare(String[] o1, String[] o2) {
+                    return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+                }
+            });
+            for (String[] i : arr) {
+                bw.write(i[0] + " " + i[1] + "\n");
+            }
+
+            bw.flush();
+            bw.close();
+            br.close();
+        }
+    }
+}
