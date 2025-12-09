36ded2dd5dd713e1cd455109310b3c64396f04f7
KimYoonMin <dbsalszz@naver.com>
2025-12-09 11:42:59 +0900

    [Recursive] BJ15655 문제 풀이 추가 (개인 문제 풀이, 통과 완료)

---
+++ Implementation/Recursive/BJ15655.java
@@ -0,0 +1,62 @@
+package Implementation.Recursive;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BJ15655 {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] arr;
+    static int m;
+
+    static boolean isAscending(List<Integer> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i - 1) > list.get(i)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public static void recursive(List<Integer> selected) throws IOException {
+        if (selected.size() == m) {
+            if (!isAscending(selected)) {
+                return;
+            }
+            for (Integer i : selected) {
+                bw.write(String.valueOf(i) + " ");
+            }
+            bw.write("\n");
+//            selected.remove(m - 1);
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (!selected.contains(arr[i])) {
+                selected.add(arr[i]);
+                recursive(selected);
+                selected.remove(selected.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] info = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = info[0];
+        m = info[1];
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            List<Integer> selected = new ArrayList<>();
+            selected.add(arr[i]);
+            recursive(selected);
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
