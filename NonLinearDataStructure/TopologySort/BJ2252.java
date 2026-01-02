6f5aae809c45fba95d6c203961bb0e0c3b3b635e
KimYoonMin <dbsalszz@naver.com>
2026-01-02 23:07:55 +0900

    [TopologySort] BJ2252 문제 풀이 추가 (정답 풀이)

--- NonLinearDataStructure/TopologySort/BJ2252.java
+++ NonLinearDataStructure/TopologySort/BJ2252.java
@@ -1,56 +1,57 @@
 package NonLinearDataStructure.TopologySort;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class BJ2252 {
-    static List<Integer>[] arr;
-    static boolean[] visited;
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int[] indegree;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int[] info = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int n = info[0];
         int m = info[1];
-        arr = new List[n + 1];
-        indegree = new int[n + 1];
-        visited = new boolean[n + 1];
+        List<Integer>[] graph = new List[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+//        int[][] graph = new int[n + 1][n + 1];
+        int[] indegree = new int[n + 1];
+        boolean[] visited = new boolean[n + 1];
         for (int i = 0; i < m; i++) {
-            int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int a = nums[0];
-            int b = nums[1];
-            arr[a].add(b);
+            int[] compare = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int a = compare[0];
+            int b = compare[1];
+            graph[a].add(b);
             indegree[b]++;
         }
-        Queue<Integer> q = new LinkedList<>();
+
+        Queue<Integer> queue = new ArrayDeque<>();
         for (int i = 1; i <= n; i++) {
             if (indegree[i] == 0) {
-                q.offer(i);
+                queue.add(i);
             }
         }
-
-        while (!q.isEmpty()) {
-            int now = q.poll();
-            visited[now] = true;
-            bw.write(now + " ");
-            for (int next : arr[now]) {
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            bw.write(node + " ");
+            visited[node] = true;
+            List<Integer> row = graph[node];
+            for (int next : row) {
+                //!visited[next] &&
                 if (visited[next]) {
                     continue;
                 }
-                indegree[next]--;
-                if (indegree[next] == 0) {
-                    q.add(next);
-
+                if (next != 0 && indegree[next] > 0) {
+                    indegree[next]--;
                 }
-
+                if (indegree[next] == 0) {
+                    queue.add(next);
+                }
             }
         }
+
 
         bw.flush();
         bw.close();
