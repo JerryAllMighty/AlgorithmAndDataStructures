dfa9c2dbd4fd40ef63db5ea4d9ec13ffe19acf53
KimYoonMin <dbsalszz@naver.com>
2026-01-14 17:15:16 +0900

    [String] BJ1157 이전 문제 풀이 추가

--- Implementation/String/BJ1157.java
+++ Implementation/String/BJ1157.java
@@ -54,18 +54,6 @@ public class Main {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
  */
 
 //2025.10.26 다른 문제풀이 추가
@@ -131,3 +119,36 @@ public class Main {
 //    }
 //}
 }
+
+
+
+//public static int getAlphabetCount(String str, char alp){
+//    int count = 0;
+//    for(int i =0; i < str.length(); i++){
+//        if(alp == str.charAt(i))count++;
+//
+//    }
+//    return count;
+//
+//}
+//public static void Test(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    String str = sc.next().toUpperCase();
+//
+//    int maxCount = -1;
+//    int maxAlphabet = '?';
+//
+//    for(char alp = 'A'; alp <= 'Z';alp++){
+//        int count = getAlphabetCount(str, alp);
+//
+//        if(count > maxCount){
+//            maxCount = count;
+//            maxAlphabet = alp;
+//        }else if(count == maxCount){
+//            maxAlphabet = '?';
+//        }
+//    }
+//
+//    System.out.println((char)(maxAlphabet));
+//
+//}
