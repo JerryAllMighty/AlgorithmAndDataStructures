92fe06f89b92640a963f308616fe8f23a61d5137
KimYoonMin <dbsalszz@naver.com>
2026-01-14 16:18:32 +0900

    [String] BJ1543 문제 풀이 완료

--- Implementation/String/BJ1543.java
+++ Implementation/String/BJ1543.java
@@ -1,31 +1,38 @@
 package Implementation.String;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+public class BJ1543 {
+    /*
+    * 2026.01.14 개인 문제풀이, 통과
+    * package com.main.javacompile;
 
+import java.io.*;
 
-public class Test {
-    public static void Test(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
-        String b = sc.nextLine();
-        int idx = 0;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String document = br.readLine();
+        String target = br.readLine();
         int answer = 0;
-        while (idx + b.length() <= a.length()) {
-            String s = a.substring(idx, idx + b.length());
-            if (s.equals(b)) {
+        int index = 0;
+        while (index <= document.length() - target.length()) {
+            if (document.substring(index, index + target.length()).equals(target)) {
                 answer++;
-                idx += b.length();
+                index += target.length();
             } else {
-                idx++;
+                index++;
             }
         }
-        System.out.println(answer);
 
 
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+        br.close();
     }
+}
+     */
 }
 //2025.10.26 다른 문제풀이 추가
 //package com.main.javacompile;
