caccb194d0ee349810b4ce4b6d2c379ece84f81b
KimYoonMin <dbsalszz@naver.com>
2026-01-19 12:57:27 +0900

    [Array] BJ 3273 문제 풀이 추가 (개인 풀이, 통과 완료)

--- LinearDataStructure/Array/BJ3273.java
+++ LinearDataStructure/Array/BJ3273.java
@@ -5,6 +5,30 @@ import java.util.Arrays;
 
 public class BJ3273 {
 
+    /*
+     * 2026.01.19 다시 풀이, 통과 완료
+     *   public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int x = Integer.parseInt(br.readLine());
+        int answer = 0;
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            int index = Arrays.binarySearch(arr, x - arr[i]);
+            if (i < index) {
+                answer++;
+            }
+        }
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+     */
+
     static int binarySearch(int[] arr, int key) {
         int l = 0;
         int r = arr.length - 1;
