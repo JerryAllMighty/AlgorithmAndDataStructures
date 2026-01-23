361d81949ae1b89dfa1c63a2e917662a6c88c944
KimYoonMin <dbsalszz@naver.com>
2026-01-23 13:10:11 +0900

    [DP] BJ 2156 문제 풀이 추가 (개인 풀이, 통과 못함)

--- ProblemSolvingSkills/DP/BJ2156.java
+++ ProblemSolvingSkills/DP/BJ2156.java
@@ -5,6 +5,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BJ2156 {
+    /*
+    * 2026.01.23 개인풀이, 40분, 통과 못 함
+    * public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] juices = new int[n];
+        int[] dp = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            int juiceAmount = Integer.parseInt(br.readLine());
+            juices[i] = juiceAmount;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n; j++) {
+                int juiceAmount = juices[i];
+//                dp[i] = dp[]
+            }
+
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+     */
 
     static List<Integer> indexes = new ArrayList<>();
 
