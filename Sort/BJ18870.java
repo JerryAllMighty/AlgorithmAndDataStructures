16797034694609f885fc3a840dc11d7e304bb40f
KimYoonMin <dbsalszz@naver.com>
2026-01-26 16:00:40 +0900

    feat(Sort): BJ 18870 문제 풀이 추가  - 개인 풀이, 통과 완료

--- Sort/BJ18870.java
+++ Sort/BJ18870.java
@@ -4,6 +4,33 @@ import java.io.*;
 import java.util.*;
 
 public class BJ18870 {
+    /*
+     * 2026.01.26 개인 문제 풀이 완료
+     *  public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Set<Integer> set = new TreeSet<>();
+        for (int i : arr) {
+            set.add(i);
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        StringJoiner joiner = new StringJoiner(" ");
+        int index = 0;
+        for (int i : set) {
+            map.put(i, index++);
+            joiner.add(String.valueOf(index));
+        }
+        for (int i = 0; i < n; i++) {
+            joiner.add(String.valueOf(map.get(arr[i])));
+        }
+        bw.write(joiner.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+     */
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
