b0b9665a81a4faaddb37517b1dfa313f60c98c81
KimYoonMin <dbsalszz@naver.com>
2026-01-27 12:52:34 +0900

    feat(Prefix Sum): BJ 11659 문제 풀이 추가  - 개인 풀이, 통과 완료

--- ProblemSolvingSkills/PrefixSum/BJ11659.java
+++ ProblemSolvingSkills/PrefixSum/BJ11659.java
@@ -4,6 +4,32 @@ import java.io.*;
 import java.util.Arrays;
 
 public class BJ11659 {
+    /*
+    * 2026.01.27 개인 문제 풀이, 통과 완료
+    * public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = input[0];
+        int m = input[1];
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] acc = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            acc[i] = acc[i - 1] + arr[i - 1];
+        }
+        for (int k = 0; k < m; k++) {
+            int[] scope = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int i = scope[0];
+            int j = scope[1];
+            bw.write(String.valueOf(acc[j] - acc[i - 1]) + "\n");
+
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
