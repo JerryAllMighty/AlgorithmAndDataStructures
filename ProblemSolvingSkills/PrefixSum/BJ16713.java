3c9781d3513229d4713661a256af5d3bd1066be5
KimYoonMin <dbsalszz@naver.com>
2026-01-27 14:03:12 +0900

    feat(Prefix Sum): BJ 16713 문제 풀이 추가  - 개인 풀이, 통과 완료

--- ProblemSolvingSkills/PrefixSum/BJ16713.java
+++ ProblemSolvingSkills/PrefixSum/BJ16713.java
@@ -4,6 +4,40 @@ import java.io.*;
 import java.util.Arrays;
 
 public class BJ16713 {
+    //TODO : XOR 연산자 숙지
+    /*
+    * 2026.01.27 개인 문제 풀이, 통과 못 함
+    *  public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = input[0];
+        int q = input[1];
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] acc = new int[n + 1];
+        acc[1] = arr[0];
+        for (int i = 2; i <= n; i++) {
+            acc[i] = acc[i - 1] ^ arr[i - 1];
+        }
+        int answer = -1;
+        for (int i = 0; i < q; i++) {
+            int[] scope = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int s = scope[0];
+            int e = scope[1];
+            int accXor = acc[e] - acc[s - 1];
+            if (answer == -1) {
+                answer = accXor;
+            } else {
+                answer = answer ^ accXor;
+            }
+        }
+        bw.write(String.valueOf(answer));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+     */
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
