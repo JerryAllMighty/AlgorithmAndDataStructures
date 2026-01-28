bcca2a18293056d09d05212faad659e98ccc61d2
KimYoonMin <dbsalszz@naver.com>
2026-01-28 15:50:50 +0900

    feat(binarySearch): BJ 2110 문제 풀이 추가  - 개인 풀이, 테스트 케이스 통과 못함

--- Search/BinarySearch/BJ2110.java
+++ Search/BinarySearch/BJ2110.java
@@ -5,6 +5,64 @@ import java.util.Arrays;
 
 //TODO : 해당 클래스 다시 풀어보기
 public class BJ2110 {
+    /*
+    * 2026.01.28 개인 풀이, 테스트 케이스 통과 못 함
+    *  static int[] positions;
+
+    static void binarySearch() {
+        int l = 0;
+        int r = positions.length - 1;
+        int mid = (l + r) / 2;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            int distance = getAdjacentDistance();
+            if () {
+
+            } else {
+
+            }
+
+        }
+    }
+
+    static int getAdjacentDistance() {
+        int distance = Integer.MAX_VALUE;
+        int i = 0;
+        while (i < positions.length) {
+            if (positions[i] != 0) {
+                int index = i + 1;
+                while (index < positions.length) {
+                    if (positions[index] != 0) {
+                        distance = Math.min(distance, index - i);
+                        i = index;
+                        break;
+                    }
+                    index++;
+                }
+            }
+        }
+        return distance;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = input[0];
+        int c = input[1];
+        positions = new int[n];
+        for (int i = 0; i < n; i++) {
+            int position = Integer.parseInt(br.readLine());
+            positions[i] = position;
+        }
+        Arrays.sort(positions);
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+     */
     static int[] houses;
     static int c;
 
