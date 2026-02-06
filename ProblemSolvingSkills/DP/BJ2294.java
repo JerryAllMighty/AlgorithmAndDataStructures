6bc03201d5af11d94a791825fe80e949c4a4e02a
KimYoonMin <dbsalszz@naver.com>
2026-02-06 13:49:58 +0900

    feat(dp): BJ 2294 문제 풀이 추가

--- ProblemSolvingSkills/DP/BJ2294.java
+++ ProblemSolvingSkills/DP/BJ2294.java
@@ -5,6 +5,48 @@ import java.util.Arrays;
 
 public class BJ2294 {
     /*
+    * 2026.02.06 3트, 개인 문제 풀이, 40분, 틀림
+    * public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] info = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = info[0];
+        int k = info[1];
+        int[] coins = new int[n];
+        int[] dp = new int[100001];
+        for (int i = 0; i < n; i++) {
+            int coin = Integer.parseInt(br.readLine());
+            coins[i] = coin;
+            dp[coin] = 1;
+        }
+
+        for (int i = 1; i <= k; i++) {
+            for (int j = 0; j < n; j++) {
+                int coin = coins[j];
+                if (coin > i) {
+                    continue;
+                }
+                if (dp[i] == 0) {
+                    dp[i] = dp[i - coin] + dp[coin];
+                } else {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + dp[coin]);
+                }
+            }
+        }
+
+        if (dp[k] == 0) {
+            bw.write(String.valueOf(-1));
+        } else {
+            bw.write(String.valueOf(dp[k]));
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+     */
+    /*
     * 2026.01.23 개인 문제 풀이, 40분, 통과 못 함
     *  public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -85,13 +127,3 @@ public class BJ2294 {
         br.close();
     }
 }
-/*
-* 2025.11.17 정답 코드 추가
-* for (int i = n - 1; i >= 0; i--) {
-            if (k >= values[i]) {
-                answer += moneyLeft / values[i];
-                moneyLeft %= values[i];
-            }
-
-        }
- */
