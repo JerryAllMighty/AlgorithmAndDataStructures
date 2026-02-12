3e6b1954304e5176038d4678d9282beb53aa5ec5
KimYoonMin <dbsalszz@naver.com>
2026-02-12 14:48:04 +0900

    feat(List): BJ 1406 문제 풀이 추가

--- LinearDataStructure/List/BJ1406.java
+++ LinearDataStructure/List/BJ1406.java
@@ -7,6 +7,48 @@ import java.util.List;
 import java.util.ListIterator;
 
 public class BJ1406 {
+    /*
+    * 2026.02.12 개인 문제 풀이, 틀림
+    *  public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        List<Character> word = new ArrayList<>();
+        char[] input = br.readLine().toCharArray();
+        for (char c : input) {
+            word.add(c);
+        }
+        ListIterator<Character> iterator = word.listIterator(word.size());
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String[] commands = br.readLine().split(" ");
+            String command = commands[0];
+            if ("P".equals(command)) {
+                iterator.add(commands[1].charAt(0));
+            } else if ("L".equals(command)) {
+                if (iterator.hasPrevious()) {
+                    iterator.previous();
+                }
+            } else if ("D".equals(command)) {
+                if (iterator.hasPrevious()) {
+                    iterator.next();
+                }
+            } else if ("B".equals(command)) {
+                if (iterator.hasPrevious()) {
+                    iterator.previous();
+                    iterator.remove();
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Character c : word) {
+            sb.append(c);
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
