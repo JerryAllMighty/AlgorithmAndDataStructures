11e657efc3ac30f8aad703d69e831615d180a521
KimYoonMin <dbsalszz@naver.com>
2026-02-28 15:06:30 +0900

    feat(String): BJ 4949 문제 풀이 추가 - 개인 문제 풀이 - 통과 완료

--- LinearDataStructure/Stack/BJ4949.java
+++ LinearDataStructure/Stack/BJ4949.java
@@ -7,6 +7,51 @@ import java.util.Deque;
 import java.util.List;
 
 public class BJ4949 {
+    /*
+    * 2026.02.28 개인 문제풀이, 통과 완료
+    * import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while (true) {
+            String sentence = br.readLine();
+            if (".".equals(sentence)) {
+                break;
+            }
+            String[] word = sentence.split("");
+            Stack<String> parenthesisStack = new Stack<>();
+            boolean isBalance = true;
+            for (String s : word) {
+                if ("(".equals(s) || "[".equals(s)) {
+                    parenthesisStack.add(s);
+                } else if (")".equals(s)) {
+                    if (parenthesisStack.isEmpty() || !"(".equals(parenthesisStack.peek())) {
+                        isBalance = false;
+                        break;
+                    }
+                    parenthesisStack.pop();
+                } else if ("]".equals(s)) {
+                    if (parenthesisStack.isEmpty() || !"[".equals(parenthesisStack.peek())) {
+                        isBalance = false;
+                        break;
+                    }
+                    parenthesisStack.pop();
+                }
+            }
+            if (!isBalance) {
+                bw.write("no\n");
+            } else {
+                bw.write(parenthesisStack.isEmpty() ? "yes\n" : "no\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
