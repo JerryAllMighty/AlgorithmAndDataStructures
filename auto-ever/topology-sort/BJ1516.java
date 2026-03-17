3818bbc3412c7c84c0ca9e7bedf99dff471f2ea0
KimYoonMin <dbsalszz@naver.com>
2026-03-17 17:54:36 +0900

    feat(topology-sort): BJ 1516 풀이 추가 - 복기 틀림

--- auto-ever/topology-sort/BJ1516.java
+++ auto-ever/topology-sort/BJ1516.java
@@ -1,4 +1,95 @@
 /*
+ * 2026 03 17 복기 틀림
+ * import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] answers;
+    static Set<Integer>[] adjacentList;
+    static int[] inDegree;
+    static int[] weights;
+    static List<Integer>[] preceedingList;
+
+
+    static void search() {
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i <= n; i++) {
+            if (inDegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+        answers[1] = weights[1];
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            for (Integer i : adjacentList[current]) {
+                if (--inDegree[i] == 0) {
+                    queue.add(i);
+                    int maximunProceeding = Integer.MIN_VALUE;
+                    for (Integer proceeding : preceedingList[i]) {
+                        maximunProceeding = Math.max(maximunProceeding, proceeding);
+                    }
+                    answers[i] = maximunProceeding + weights[i];
+                }
+
+            }
+
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
+        answers = new int[n + 1];
+        Arrays.fill(answers, Integer.MAX_VALUE);
+        adjacentList = new HashSet[n + 1];
+        preceedingList = new List[n + 1];
+        for (int i = 1; i <= n; i++) {
+            adjacentList[i] = new HashSet<>();
+            preceedingList[i] = new ArrayList<>();
+        }
+        inDegree = new int[n + 1];
+        weights = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+            int time = Integer.parseInt(stringTokenizer.nextToken());
+            weights[i] = time;
+            inDegree[i]++;
+            int nextVertex = i;
+            while (true) {
+                int adjacent = Integer.parseInt(stringTokenizer.nextToken());
+                if (adjacent == -1) {
+                    break;
+                }
+                adjacentList[adjacent].add(nextVertex);
+                preceedingList[i].add(adjacent);
+                nextVertex = adjacent;
+            }
+        }
+
+        search();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            stringBuilder.append(String.valueOf(answers[i]));
+            stringBuilder.append("\n");
+        }
+        bw.write(stringBuilder.toString());
+        bw.flush();
+        bw.close();
+    }
+}
+
+
+
+
+
+
+ */
+/*
  * 2026 03 17 개인 문제 풀이, 틀림
  * import java.io.*;
 import java.util.*;
