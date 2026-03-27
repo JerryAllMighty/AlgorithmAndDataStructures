88648f24a75250a81680835b2b3038cf97ed366c
KimYoonMin <dbsalszz@naver.com>
2026-03-27 11:36:45 +0900

    feat(dijkstra): BJ 1504 풀이 추가 - 복기 풀이 완료

--- auto-ever/dijkstra/BJ1504.java
+++ auto-ever/dijkstra/BJ1504.java
@@ -1,4 +1,112 @@
 /*
+ * 2026 03 27 복기 풀이 완료
+ * import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int e;
+    static int v1;
+    static int v2;
+    static List<Node>[] graph;
+    static int[] shortestPath;
+    static final int INF = 200000000;
+
+    static class Node implements Comparable<Node> {
+        private int targetNode;
+        private int weight;
+
+        public Node(int targetNode, int weight) {
+            this.targetNode = targetNode;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.weight - o.weight;
+        }
+    }
+
+    static long search(int start, int end) {
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(start, 0));
+
+        Arrays.fill(shortestPath, Integer.MAX_VALUE);
+        shortestPath[start] = 0;
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            int currentNode = current.targetNode;
+            int currentWeight = current.weight;
+
+            if (shortestPath[currentNode] < currentWeight) {
+                continue;
+            }
+
+            List<Node> nodeList = graph[currentNode];
+            for (Node node : nodeList) {
+                if (shortestPath[node.targetNode] > shortestPath[currentNode] + node.weight) {
+                    shortestPath[node.targetNode] = shortestPath[currentNode] + node.weight;
+                    queue.add(new Node(node.targetNode, shortestPath[node.targetNode]));
+                }
+            }
+        }
+
+        return shortestPath[end];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(stringTokenizer.nextToken());
+        e = Integer.parseInt(stringTokenizer.nextToken());
+        graph = new List[n + 1];
+        shortestPath = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < e; i++) {
+            stringTokenizer = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+            int b = Integer.parseInt(stringTokenizer.nextToken());
+            int c = Integer.parseInt(stringTokenizer.nextToken());
+            graph[a].add(new Node(b, c));
+            graph[b].add(new Node(a, c));
+        }
+        stringTokenizer = new StringTokenizer(br.readLine());
+        v1 = Integer.parseInt(stringTokenizer.nextToken());
+        v2 = Integer.parseInt(stringTokenizer.nextToken());
+
+        long result1 = 0;
+        result1 += search(1, v1);
+        result1 += search(v1, v2);
+        result1 += search(v2, n);
+
+        long result2 = 0;
+        result2 += search(1, v2);
+        result2 += search(v2, v1);
+        result2 += search(v1, n);
+
+        long answer = Math.min(result1, result2);
+        if (result1 >= INF && result2 >= INF) {
+            bw.write("-1");
+        } else {
+            bw.write(String.valueOf(answer));
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
+
+
+
+
+
+
+ */
+/*
  * 2026 03 24 복기 풀이 실패
  * import java.io.*;
 import java.util.*;
