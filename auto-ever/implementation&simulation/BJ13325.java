4426797fd9eb6973dacfe0f013762dfb5e2c6afa
KimYoonMin <dbsalszz@naver.com>
2026-04-01 18:36:36 +0900

    feat(simulation): BJ 13325 풀이 추가 - 개인 풀이 통과 완료

--- auto-ever/implementation&simulation/BJ13325.java
+++ auto-ever/implementation&simulation/BJ13325.java
@@ -1,4 +1,60 @@
 /*
+ * 2026 04 01 개인 문제 풀이 통과 완료
+ * import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int w = Integer.parseInt(stringTokenizer.nextToken());
+        int L = Integer.parseInt(stringTokenizer.nextToken());
+        stringTokenizer = new StringTokenizer(br.readLine());
+//        int[] trucks = new int[n];
+        Queue<Integer> trucks = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            int truck = Integer.parseInt(stringTokenizer.nextToken());
+            trucks.offer(truck);
+        }
+        int answer = 0;
+        int currentLoaded = 0;
+        Deque<Integer> currentTrucks = new ArrayDeque<>(w);
+        for (int i = 0; i < w; i++) {
+            currentTrucks.offer(0);
+        }
+        while (true) {
+            currentLoaded -= currentTrucks.pollFirst();
+            if(!trucks.isEmpty()){
+                int expectedTruck = trucks.peek();
+                if (currentLoaded + expectedTruck <= L) {
+                    int newTruck = trucks.poll();
+                    currentTrucks.offer(newTruck);
+                    currentLoaded += newTruck;
+                }else{
+                    currentTrucks.offer(0);
+                }
+            }
+
+            answer++;
+            if (trucks.isEmpty() && currentTrucks.isEmpty()) {
+                break;
+            }
+        }
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+    }
+}
+
+
+
+
+
+
+ */
+/*
 * 2026.03.08, 개인 문제 풀이, 테케 통과 못함, 복기 때 통과 성공
 * 시간 당 수행되어야하는 작업 단위 분리, 큐를 다 뽑고 나서 0을 큐에 적재하는 분기를 구분 못했음
 * import java.io.*;
