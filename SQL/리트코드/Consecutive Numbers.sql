89baa043e15da17a0b1b595a16bf65b38fc827e9
KimYoonMin <dbsalszz@naver.com>
2026-07-08 20:13:13 +0900

    feat(sql): leet code sql 문제 풀이 - Consecutive Numbers

---
+++ SQL/리트코드/Consecutive Numbers.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+with recursive cte as (
+    select
+    id
+    , num
+    , 1 as ConsecutiveNums
+    from Logs
+    where id = 1
+    union all
+    select
+    l.id
+    , l.num
+    , if(l.num = c.num, ConsecutiveNums + 1, 1) as ConsecutiveNums
+    from cte c
+    inner join Logs l on l.id = c.id + 1
+)
+
+select num as ConsecutiveNums
+from cte
+where ConsecutiveNums  >= 3
