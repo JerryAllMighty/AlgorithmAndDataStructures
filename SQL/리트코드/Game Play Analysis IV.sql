2c9fcab4a3097260279ec94383ec2351cd15e145
KimYoonMin <dbsalszz@naver.com>
2026-07-09 19:54:44 +0900

    feat(sql): leet code sql 문제 풀이 - Game Play Analysis IV

--- SQL/리트코드/Game Play Analysis IV.sql
+++ SQL/리트코드/Game Play Analysis IV.sql
@@ -1,7 +1,34 @@
+with cte as (
+    select
+        player_id
+         ,device_id
+         ,event_date
+         ,games_played
+         , lead(event_date) over(partition by player_id order by event_date asc) as next_event_date
+    , row_number() over(partition by player_id order by event_date asc) as rn
+    from Activity
+)
+   , validCount as (
+    select
+        player_id
+         , case when
+                    datediff(next_event_date, event_date) = 1 then 1
+                else 0
+        end as cnt
+    from cte
+    where rn = 1
+)
+
+
 select
-    player_id
-     ,device_id
-     ,event_date
-     ,games_played
-     , count(player_id) over(partition by player_id order by event_date) as cnt
-from Activity
+    round((
+              select count(*)
+              from validCount
+              where cnt = 1
+          ) /
+          (
+              select count(distinct player_id)
+              from Activity
+          ), 2
+    ) as fraction
+
